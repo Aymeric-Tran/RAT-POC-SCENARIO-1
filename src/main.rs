36a4b9acb94b8585d6ac5902b0364aa4028d6ac2
Kaioken <raygsm@live.fr>
2024-12-28 17:23:33 +0100

    test simple de connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(_stream) = TcpStream::connect("127.0.0.1:8000") {
+        println!("Connecté au serveur");
+    } else {
+        println!("Impossible de se connecter au serveur");
+    }
 }
