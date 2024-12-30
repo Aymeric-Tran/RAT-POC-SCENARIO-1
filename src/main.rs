26b97cf21fcf687dbb25e44b4fd2476ff6bf9e91
Kaioken <raygsm@live.fr>
2024-12-30 15:32:22 +0100

    test communication serveur

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use std::net::TcpStream;
+use tokio::net::TcpStream;
+use tokio::io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 
-fn main() {
-    if let Ok(_stream) = TcpStream::connect("127.0.0.1:8000") {
-        println!("Connecté au serveur");
-    } else {
-        println!("Impossible de se connecter au serveur");
+const C2_ADDR: &str = "192.168.9.136:8080";
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let server_address = C2_ADDR;
+    let mut stream = TcpStream::connect(server_address).await?;
+
+    println!("Connected to the server at {}", server_address);
+
+    loop {
+        let mut reader = BufReader::new(io::stdin());
+
+        println!("Enter some text :");
+
+        let mut msg = String::new();
+        
+        reader.read_line(&mut msg).await?;
+
+        stream.write_all(msg.as_bytes()).await?;
+    
+        // let mut buffer = [0; 1024];
+        // let n = stream.read(&mut buffer).await?;
+        
+        // let received = match std::str::from_utf8(&buffer[..n]) {
+        //     Ok(v) => v,
+        //     Err(e) => {
+        //         println!("Failed {}", e);
+        //         return Ok(())
+        //     }
+        // };
+    
+        // println!("Received : {}", received);
     }
 }
