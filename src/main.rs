764049f768fcfae633a3c5f75769f4bdd523b3de
Kaioken <raygsm@live.fr>
2025-02-27 18:01:29 +0100

    ajout recepetion commandes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Directive(Vec<String>);
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://127.0.0.1:3030/directives";
+
+    let client = reqwest::Client::builder()
+    .danger_accept_invalid_certs(true)
+    .build()?;
+
+    let response = client.get(url).send().await?;
+
+    if response.status().is_success() {
+        let commands: Directive = response.json().await?;
+        for command in commands.0.iter() {
+            println!("{}", command)
+        }
+    } else {
+        println!("Error {}", response.status());
+    }
+
+    Ok(())
 }
